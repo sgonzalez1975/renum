3631065785a1a0f12e0a34a751799482423eb25b
Sergio Gonzalez <sgonzalez1975@gmail.com>
2024-06-27 00:27:13 -0600

    ahora soporta guiones antes del número.

--- rezero.py
+++ rezero.py
@@ -3,31 +3,34 @@ import re
 import os
 
 path = os.getcwd()
-arr = [f.name for f in os.scandir(path) if f.is_file() and f.name[-1]!='t' and f.name[-1]!='y']
+arr = [f.name for f in os.scandir(path) if f.is_file() and f.name[-1] not in ['t', 'y', 'd']]
 
 tam_max = 0
 cambs = []
 for nom in arr:
-    pos = nom.find('.')
+    pos = nom.rfind('.')
     if (pos < 0):
         continue
-    pos2 = nom.find('-')
-    if (pos2 > 0 and pos2 < pos):
-        pos = pos2
-    if (pos>tam_max):
+    match = re.search(r'\d', nom)
+    pos_n = match.start()
+    #print(pos_n)
+    if (pos_n < 0):
+        continue    
+    pos2 = nom[pos_n:].find('-')
+    if (pos2 > 0 and pos2 + pos_n < pos):
+        pos = pos2 + pos_n
+    if (pos > tam_max):
         tam_max = pos
-    cambs.append([nom, pos])
-#print(tam)
+    cambs.append([nom, pos, pos_n])
+#print(tam_max)
 origs = []
-for nom, tam  in cambs:
+for nom, tam, pos_n in cambs:
     cant = tam_max - tam
     if (cant > 0):
         #rellena de ceros
-        match = re.search(r'\d', nom)
-        if match.start() >= 0:
-            nomdest = nom[:match.start()] + '0'*cant + nom[match.start():]
-            print(nom, "=>", nomdest)
-            os.rename(nom, nomdest)
+        nomdest = nom[:pos_n] + '0'*cant + nom[pos_n:]
+        print(nom, "=>", nomdest)
+        os.rename(nom, nomdest)
     else:
         origs.append(nom)
 print("sin cambios:", origs)
